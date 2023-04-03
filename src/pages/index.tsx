5e2e58de4f18188cfa9d122cbda642d6bde5518e
rmolines <rafaelmismael@gmail.com>
2023-04-03 15:53:29 -0300

    asdf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -115,7 +115,7 @@ export default function Home() {
 	const descriptions = [
 		{
 			title: "VagasEmStartups.com",
-			description: "Job at startups from the best VC funds in Brazil",
+			description: "Jobs at startups from the best VC funds in Brazil",
 			logoPath: "/vagas.png",
 			videoPath: vagasScreenShot,
 			type: "screenshot",
